025afd1f13d299d5c4d25ee28ddc42180136f0f8
Dev-Ty <88804937+TEE2DWHY@users.noreply.github.com>
2024-05-11 21:19:43 +0100

    Create remix.sol

---
+++ remix.sol
@@ -0,0 +1,29 @@
+//  Remix Simple Storage
+// SPDX-License-Identifier: MIT  (first rule of thumb is to declare the SPDX License)
+pragma solidity ^0.8.19; // (secondly declare the solidity version)
+
+//*** (Local and Global Scope)
+// funciton types: Getters and Setters
+
+contract SimpleStorage {
+    // we just initialized our contract
+    int256 bestNumber; // ?? this hardcodes the value of best number - Global Scoped Variable
+
+    function setBestNumber(int256 _newBestNumber, int256 _newAge) public {
+        //write method
+        bestNumber = _newBestNumber;
+        int256 age = 20;
+        age = _newAge; // local scope variable
+    }
+
+    function getBestNumber() public view returns (int256) {
+        // read method (use the retun keyword)
+        return bestNumber;
+    }
+}
+
+// in solidity we have diffrent kind of identifiers:
+// 1. Public Identifiers
+// 2. Private Identifiers
+// 3. External Identifiers
+// 4. Internal Identifiers
