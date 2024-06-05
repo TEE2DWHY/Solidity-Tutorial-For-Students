47f2df3f39b14fa9b90ec1402c8cea7e5983b74b
Dev-Ty <88804937+TEE2DWHY@users.noreply.github.com>
2024-06-05 19:18:42 +0100

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -34,3 +34,31 @@ contract PriceConverter {
        return amountInUsd;
     }
 }
+
+
+// using price converter as 
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.7;
+
+// import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+// library PriceConverter { 
+//     function getPrice() public view returns (uint256) {
+//         AggregatorV3Interface priceFeed = AggregatorV3Interface( 0x694AA1769357215DE4FAC081bf1f309aDC325306);
+//         (
+//             /* uint80 roundID */,
+//             int answer, 
+//             /*uint startedAt*/,
+//             /*uint timeStamp*/,
+//             /*uint80 answeredInRound*/
+//         ) = priceFeed.latestRoundData();
+//         return uint256(answer * 1e10); //18  [18]
+//     }
+
+//     function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+//        uint256 ethPrice = getPrice();
+//        uint256 amountInUsd = ethAmount * ethPrice / 1e18 ; //(we have to divide by 1e18 since eth price is returned in wei (1e18)
+//        return amountInUsd;
+//     }
+// }
+
