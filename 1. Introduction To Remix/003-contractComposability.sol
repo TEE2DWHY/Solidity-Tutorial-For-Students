e36be4ed51ba0e40e504c22433b26e861cace5be
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-06-06 20:05:47 +0100

    modified the readMe file

--- 1. Introduction To Remix/003-contractComposability.sol
+++ 1. Introduction To Remix/003-contractComposability.sol
@@ -3,32 +3,30 @@ pragma solidity ^0.8.9;
 
 // CONTRACT_COMPOSABILITY
 
-import "./SimpleStorage.sol"; //import simple storage 
+import "./002-SimpleStorage.sol"; //import simple storage
 
 contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
     SimpleStorage simpleStorage = new SimpleStorage();
 
-
-    function createSimpleStorageContract() public{
+    function createSimpleStorageContract() public {
         simpleStorageArray.push(simpleStorage);
     }
 
     function setBestNumber(int256 _bestNumber) public {
-       simpleStorage.setBestNumber(_bestNumber); // called the setBestNumber from the simple storage contract right here in StorageFactory
+        simpleStorage.setBestNumber(_bestNumber); // called the setBestNumber from the simple storage contract right here in StorageFactory
     }
 
-    function getBestNumber() public view  returns (int256){
-       int256 bestNumber =  simpleStorage.getBestNumber(); // called the getBestNumber from the simple storage contract right here in StorageFactory
-       return bestNumber;
+    function getBestNumber() public view returns (int256) {
+        int256 bestNumber = simpleStorage.getBestNumber(); // called the getBestNumber from the simple storage contract right here in StorageFactory
+        return bestNumber;
     }
 }
 
-
-// Contract composability refers to the ability of different smart contracts to seamlessly interact and work together within a blockchain ecosystem. 
+// Contract composability refers to the ability of different smart contracts to seamlessly interact and work together within a blockchain ecosystem.
 // In simpler terms, it's about creating modular, reusable, and interoperable smart contracts that can be combined like building blocks to achieve more complex functionalities or applications.
 
-// Composability is a key feature of many blockchain platforms, particularly Ethereum. 
+// Composability is a key feature of many blockchain platforms, particularly Ethereum.
 // Smart contracts on Ethereum are designed to be composable, meaning they can call functions or interact with other smart contracts, allowing developers to build sophisticated decentralized applications (DApps) by combining existing smart contracts with new ones.
 
 // The benefits of contract composability include:
