b7adb44295d3244a62be3aaa95ebcd14c7f7e28e
TEE2DWHY <isaacolorunfemi330@gmail.com>
2024-06-08 12:19:17 +0100

    completed the test for the delete task function

--- 2. Moving to Hardhat/contracts/ToDoList.sol
+++ 2. Moving to Hardhat/contracts/ToDoList.sol
@@ -56,6 +56,13 @@ contract ToDoList {
         address indexed owner
     );
 
+    event DeletedTask(
+        uint256 taskId,
+        string indexed taskName,
+        bool isTaskCompleted,
+        address indexed owner
+    );
+
     function createTask(uint256 _taskId, string memory _taskName) public {
         // taskId
         //what task?
@@ -72,15 +79,41 @@ contract ToDoList {
         emit CreatedTask(_taskId, _taskName, task.isTaskCompleted, msg.sender);
     }
 
-    function updateTaskState(uint256 _taskId, bool currentState) public {
+    function completed(uint256 _taskId) public {
         // we need to get the task;
         // isTaskCompleted = true;
-        Task memory task = getTaskByTaskId[_taskId][msg.sender];
+        Task storage task = getTaskByTaskId[_taskId][msg.sender]; // get task associated with user by taskId
+        //(task here is used as storage not memory because we want to directly updates the task's completion status in storage, reflecting the change in the contract's state)
         if (task.isTaskCompleted) {
-            revert TaskAlreadyCompleted();
+            revert TaskAlreadyCompleted(); // revert with custom error just incase the task has already been marked as completed
         }
-        getTaskByTaskId[_taskId][msg.sender].isTaskCompleted = currentState;
-        emit UpdatedTask(
+        task.isTaskCompleted = true; // update task state to true
+        emit UpdatedTask( // emit event
+            task.id,
+            task.taskName,
+            task.isTaskCompleted,
+            task.owner
+        );
+    }
+
+    function deleteTask(uint256 _taskId) public {
+        // we need to iterate through all the available task
+        // we need to check if the taskId matches any available tasks with the same taskId
+        // if true we need to move the task to be deleted to the last on the user task array
+        // we need to use the .pop method to delete the task
+        Task memory task = getTaskByTaskId[_taskId][msg.sender]; // get the task that matches the taskId by user
+        for (_taskId = 0; _taskId < listOfTasks[msg.sender].length; _taskId++) {
+            //loop through the user tasks
+            if (listOfTasks[msg.sender][_taskId].id == task.id) {
+                // check if the taskId matches the list of user tasksIds
+                listOfTasks[msg.sender][_taskId] = listOfTasks[msg.sender][
+                    listOfTasks[msg.sender].length - 1
+                ]; // move the task that matches the taskId to the end of the user task array
+                listOfTasks[msg.sender].pop(); // remove the task using the pop method
+                break; // terminate the loop as there is no need to keep iterating
+            }
+        }
+        emit DeletedTask( //emit the deleted task
             task.id,
             task.taskName,
             task.isTaskCompleted,
--- 2. Moving to Hardhat/test/ToDoList.test.js
+++ 2. Moving to Hardhat/test/ToDoList.test.js
@@ -5,51 +5,86 @@ const { assert, expect } = require("chai");
 !developmentChains.includes(network.name)
   ? describe.skip
   : describe("ToDoList", async () => {
-      let contract;
+      let ToDoList;
       let user;
       let exploiter;
 
       beforeEach(async () => {
         [user, exploiter] = await ethers.getSigners();
         const contractFactory = await ethers.getContractFactory("ToDoList");
-        contract = await contractFactory.deploy();
+        ToDoList = await contractFactory.deploy();
       });
 
       describe("Get Task", async () => {
         beforeEach(async () => {
-          await contract.connect(user).createTask(1, "I want to code");
-          await contract
-            .connect(user)
-            .createTask(2, "I want to visit the spa today");
+          await ToDoList.connect(user).createTask(1, "I want to code");
+          await ToDoList.connect(user).createTask(
+            2,
+            "I want to visit the spa today"
+          );
         });
         it("should return the task associated with a user taskId", async () => {
           const taskId = 2;
-          const task = await contract.getTaskById(taskId, user.address);
+          const task = await ToDoList.getTaskById(taskId, user.address);
           console.log(task);
         });
         it("should return all the task associated with a specific user", async () => {
-          const tasks = await contract.getAllUserTask(user.address);
+          const tasks = await ToDoList.getAllUserTask(user.address);
           console.log(tasks);
         });
       });
 
-      describe("Update Task", async () => {
+      describe("Complete Task", async () => {
         beforeEach(async () => {
-          await contract.connect(user).createTask(1, "I want to code");
+          await ToDoList.connect(user).createTask(1, "I want to code");
         });
 
-        it("should update the state of the task", async () => {
+        it("should set the state of the task to true", async () => {
           const taskId = 1;
-          await contract.connect(user).updateTaskState(taskId, true);
+          const previousTaskState = await ToDoList.getTaskById(
+            taskId,
+            user.address
+          );
+          console.log("Previous Task State:", previousTaskState);
+          await ToDoList.connect(user).completed(taskId);
+          const currentTaskState = await ToDoList.getTaskById(
+            taskId,
+            user.address
+          );
+          console.log("Current Task State", currentTaskState);
         });
 
         it("should throw an error if user tries to re-update an already completed task", async () => {
           const taskId = 1;
-          await contract.connect(user).updateTaskState(taskId, true);
-          console.log(user.address);
+          await ToDoList.connect(user).completed(taskId);
+          const currentTaskState = await ToDoList.getTaskById(
+            taskId,
+            user.address
+          );
+          console.log(currentTaskState);
           await expect(
-            contract.connect(user).updateTaskState(taskId, true)
-          ).to.be.revertedWithCustomError(contract, "TaskAlreadyCompleted");
+            ToDoList.connect(user).completed(taskId)
+          ).to.be.revertedWithCustomError(ToDoList, "TaskAlreadyCompleted");
+        });
+      });
+
+      describe("Delete Task", async () => {
+        const taskIds = [1, 2, 3]; // where taskIds[0] ----> 1, taskIds[1]-------> 2
+        beforeEach(async () => {
+          await ToDoList.connect(user).createTask(taskIds[0], "I want to code");
+          await ToDoList.connect(user).createTask(
+            taskIds[1],
+            "I want to hit the gym"
+          );
+          await ToDoList.connect(user).createTask(
+            taskIds[2],
+            "I want to make love"
+          );
+        });
+        it("should delete task", async () => {
+          await ToDoList.connect(user).deleteTask(taskIds[0]);
+          const tasks = await ToDoList.getAllUserTask(user.address);
+          console.log(tasks);
         });
       });
     });
